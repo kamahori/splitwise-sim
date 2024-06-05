b9db2abc67df34e4fef091f3b82d1f364462e144
Keisuke Kamahori <kamahori@cs.washington.edu>
2024-06-05 02:16:33 +0000

    Add network profile data

--- performance_model.py
+++ performance_model.py
@@ -260,10 +260,12 @@ class MOEPerformanceModel(PerformanceModel):
     """
     PerformanceModel for mixture of experts, based on a CSV database.
     """
-    def __init__(self, db_path):
+    def __init__(self, db_path, network_latency_path):
         super().__init__()
         self.db = pd.read_csv(os.path.join(get_original_cwd(), db_path),
                               dtype={"model": "category", "hardware": "category"})
+        self.network_latency = pd.read_csv(os.path.join(get_original_cwd(), network_latency_path),
+                              dtype={"connection": "category", "fan_in": "category"})
 
         self.db = self.db[["model",
                            "hardware",
@@ -274,11 +276,16 @@ class MOEPerformanceModel(PerformanceModel):
                            "attention_time",
                            "routing_time",
                            "expert_time"]]
+        self.network_latency = self.network_latency[["data_size",
+                                                     "connection",
+                                                     "fan_in",
+                                                     "latency"]]
 
         # convert to seconds
         self.db["attention_time"] = self.db["attention_time"] / 1000
         self.db["routing_time"] = self.db["routing_time"] / 1000
         self.db["expert_time"] = self.db["expert_time"] / 1000
+        self.network_latency["latency"] = self.network_latency["latency"] / 1000
 
         self.init_predictor()
 
@@ -289,9 +296,11 @@ class MOEPerformanceModel(PerformanceModel):
         self.attention_time_predictors = {}
         self.routing_time_predictors = {}
         self.expert_time_predictors = {}
+        self.network_time_predictors = {}
         self.attention_time_cache = {}
         self.routing_time_cache = {}
         self.expert_time_cache = {}
+        self.network_time_cache = {}
 
         for model in self.db["model"].unique():
             for hardware in self.db["hardware"].unique():
@@ -315,6 +324,19 @@ class MOEPerformanceModel(PerformanceModel):
                     y = db_subset[["batch_tokens", "expert_time"]].groupby("batch_tokens").median()["expert_time"]
                     self.expert_time_predictors[(model, hardware, tensor_parallel)] = interp1d(
                                                                     x, y, fill_value="extrapolate")
+        
+        for connection in self.db["connection"].unique():
+            for fan_in in self.db["fan_in"].unique():
+                mask = (self.network_latency["data_size"] == data_size) & \
+                        (self.network_latency["connection"] == connection) & \
+                        (self.network_latency["fan_in"] == fan_in)
+                db_subset = self.network_latency[mask].copy()
+                if len(db_subset) == 0:
+                    continue
+                x = db_subset[["data_size", "latency"]].groupby("data_size").median().index
+                y = db_subset[["data_size", "latency"]].groupby("data_size").median()["latency"]
+                self.network_time_predictors[(connection, fan_in)] = interp1d(
+                                                                x, y, fill_value="extrapolate")
 
     def _match(self, **kwargs):
         """
@@ -323,6 +345,15 @@ class MOEPerformanceModel(PerformanceModel):
         mask = True
         for k, v in kwargs.items():
             mask &= (self.db[k] == v)
+        return mask
+
+    def _match_network(self, **kwargs):
+        """
+        Returns a boolean mask for the network latency database from kwargs.
+        """
+        mask = True
+        for k, v in kwargs.items():
+            mask &= (self.network_latency[k] == v)
         return mask
 
     def predict_new_row(self, **kwargs):
@@ -345,6 +376,22 @@ class MOEPerformanceModel(PerformanceModel):
         new_row["expert_time"] = expert_time
         self.db = pd.concat([self.db, new_row], ignore_index=True)
         return new_row
+    
+    def predict_new_row_network(self, **kwargs):
+        """
+        Predicts the network latency for a new row.
+        Inserts the new row into the network latency database.
+        """
+        connection = kwargs["connection"]
+        fan_in = kwargs["fan_in"]
+        data_size = kwargs["data_size"]
+        new_row = pd.DataFrame(kwargs, index=[0])
+
+        latency = self.network_time_predictors[(connection, fan_in)](data_size)
+
+        new_row["latency"] = latency
+        self.network_latency = pd.concat([self.network_latency, new_row], ignore_index=True)
+        return new_row
 
     def get_time(self, task_time, **kwargs):
         """
@@ -357,8 +404,23 @@ class MOEPerformanceModel(PerformanceModel):
             new_row = self.predict_new_row(**kwargs)
             t_time = new_row[task_time][0]
         return t_time
+    
+    def get_network_time(self, connection, fan_in, **kwargs):
+        """
+        Returns the network latency from the database.
+        """
+        network_time = self.network_latency[
+            self._match_network(connection=connection, fan_in=fan_in)
+        ]["latency"].median()
+        # if not found, predict
+        if math.isnan(network_time):
+            new_row = self.predict_new_row_network(connection=connection, fan_in=fan_in, **kwargs)
+            network_time = new_row["latency"][0]
+        return network_time
 
-    def get_duration(self, task, batch, instance, *args, **kwargs):
+    def get_duration(self, task, batch, instance, connection="NV", fan_in=1, *args, **kwargs):
+        # TODO(keisuke): configure connection (NV and IB) and fan_in factor
+        # Also consider expert popularity
         model = instance.model.name
         hardware = instance.processors[0].name
         tensor_parallel = instance.model.parallelism.tensor_parallelism
@@ -382,7 +444,8 @@ class MOEPerformanceModel(PerformanceModel):
                                          batch_size=batch_size,
                                          token_size=token_size,
                                          batch=batch)
-            return (attention_time + routing_time) * instance.model.num_layers
+            network_time = self.get_network_time(connection=connection, fan_in=fan_in, data_size=batch_size)
+            return (attention_time + routing_time + network_time) * instance.model.num_layers
         elif task.task_type == TaskType.EXPERT:
             prompt_size = task.request.prompt_size
             token_size = task.request.token_size
@@ -395,7 +458,8 @@ class MOEPerformanceModel(PerformanceModel):
                                         batch_size=batch_size,
                                         token_size=token_size,
                                         batch=batch)
-            return expert_time * instance.model.num_layers
+            network_time = self.get_network_time(connection=connection, fan_in=fan_in, data_size=batch_size)
+            return (expert_time + network_time) * instance.model.num_layers
         else:
             raise NotImplementedError
 
